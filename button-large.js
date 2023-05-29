affe74bc208f7fe7421c9d59a5c9fba594c9820b
Martynas-J <Aurimhas@gmail.com>
2023-05-29 16:00:02 +0300

    main news done

--- button-large.js
+++ button-large.js
@@ -2,6 +2,6 @@ import { createHtmlElement } from "./functions.js";
 
 export function buttonLarge(text){
 const buttonLargeElement = createHtmlElement("a", "button-large", "#")
-buttonLargeElement.textContent = "Visos naujienos"
+buttonLargeElement.textContent = text
 return buttonLargeElement
 }
--- video-section-wrapper.js
+++ video-section-wrapper.js
@@ -1,8 +1,18 @@
+import { buttonLarge } from "./button-large.js";
 import { createHtmlElement } from "./functions.js";
 
 
 export function videoSectionWrapper() {
     const videoSection = createHtmlElement("div", "video-section-wrapper")
+    const sectionTitleElement = createHtmlElement("h2", "section-title") 
+    const videoWrapperElement = createHtmlElement("div", "video-wrapper") 
+    const videoExtraTextElement = createHtmlElement("span", "video-extra-text") 
+    const linkButtonLarge = buttonLarge("Visi vaizdo įrašai")
 
+    sectionTitleElement.textContent = "Vaizdo įrašai"
+    videoWrapperElement.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/b8dGCsP75HA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe>`
+    videoExtraTextElement.textContent = "Jei galiu aš – gali ir tu!"
+
+    videoSection.append(sectionTitleElement, videoWrapperElement, videoExtraTextElement, linkButtonLarge)
 return videoSection
 }
