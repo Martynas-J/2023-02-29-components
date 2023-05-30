f9f2cf733819d8bc82bc900ee63c1f50c4552066
Martynas-J <Aurimhas@gmail.com>
2023-05-30 08:11:26 +0300

    main news and sidebar done

--- events-list.js
+++ events-list.js
@@ -2,7 +2,7 @@ import { EVENTS_DATA } from "./config.js";
 import { event } from "./event.js";
 import { createHtmlElement } from "./functions.js";
 
-export function eventsList(data) {
+export function eventsList() {
     const eventsListElement = createHtmlElement("div", "events-list")
     EVENTS_DATA.forEach(element => {
         eventsListElement.append(event(element))
